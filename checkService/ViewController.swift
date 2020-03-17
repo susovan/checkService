277b59a6ce65bee6279a1b85270d818c88eefd90
susovan00@gmai.com <susovan00@gmail.com>
2020-03-17 13:32:20 +0530

    added variable

--- checkService/ViewController.swift
+++ checkService/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var name :String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         
         // Do any additional setup after loading the view.
