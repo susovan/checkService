c2d264b41d7175634e44ddc99b2be1c1ceab0a50
susovan00@gmai.com <susovan00@gmail.com>
2020-03-17 10:56:39 +0530

    initial commit

---
+++ CheckServiceTests/CheckServiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  CheckServiceTests.swift
+//  CheckServiceTests
+//
+//  Created by Susovan Pati on 07/11/19.
+//  Copyright © 2019 Susovan Pati. All rights reserved.
+//
+
+import XCTest
+@testable import CheckService
+
+class CheckServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ checkService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckService
+//
+//  Created by Susovan Pati on 07/11/19.
+//  Copyright © 2019 Susovan Pati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
